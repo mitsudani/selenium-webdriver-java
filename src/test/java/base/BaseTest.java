bb2bd53e3952ddf9226974c07addb64f470f418d
Mitsudani <mdsachetti@gmail.com>
2023-11-27 00:26:51 -0300

    Base test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,8 +1,25 @@
 package base;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
 
-    private WebDriver webdriver;
+    private WebDriver driver;
+
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(375, 812));
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String args[]) {
+        BaseTest test = new BaseTest();
+        test.setUp();
+    }
 }
