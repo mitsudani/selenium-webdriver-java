deeb7bbf1bc0893ea1f3acfcb1880169a1c13db7
Mitsudani <mdsachetti@gmail.com>
2023-12-02 00:58:00 -0300

    Finding web elements

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTest {
 
@@ -15,6 +19,19 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.manage().window().setSize(new Dimension(375, 812));
         System.out.println(driver.getTitle());
+
+//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+//        inputsLink.click();
+
+        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContentLink.click();
+
+        WebElement menuElementLink = driver.findElement(By.linkText("Example 1: Menu Element"));
+        menuElementLink.click();
+
+        List<WebElement> links = driver.findElements(By.xpath("//ul//li"));
+        System.out.println(links.size());
+
         driver.quit();
     }
 
