b320000de4b1c01a22c1d5ddaead8d2a21dbc1ea
Mitsudani <mdsachetti@gmail.com>
2023-12-03 00:24:17 -0300

    Element interactions

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,36 +1,43 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.Dimension;
+// import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTest {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        driver.manage().window().setSize(new Dimension(375, 812));
-        System.out.println(driver.getTitle());
+
+        homePage = new HomePage(driver);
+
+///////// These comments are just examples for practice, this type of code must be in the main folder /////////
+
+//        driver.manage().window().maximize();
+//        driver.manage().window().setSize(new Dimension(375, 812));
+//        System.out.println(driver.getTitle());
 
 //        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
 //        inputsLink.click();
 
-        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContentLink.click();
-
-        WebElement menuElementLink = driver.findElement(By.linkText("Example 1: Menu Element"));
-        menuElementLink.click();
-
-        List<WebElement> links = driver.findElements(By.xpath("//ul//li"));
-        System.out.println(links.size());
+//        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
+//        shiftingContentLink.click();
+//
+//        WebElement menuElementLink = driver.findElement(By.linkText("Example 1: Menu Element"));
+//        menuElementLink.click();
+//
+//        List<WebElement> links = driver.findElements(By.xpath("//ul//li"));
+//        System.out.println(links.size());
 
         driver.quit();
     }
